16906ce573d5ac72bdc0c8a0279ce27da2fdc47e
SamuelMarti22 <samuelmartinezarteaga22@gmail.com>
2026-03-21 18:50:47 -0500

    feat(Users): add model WishlistItem and modify model User; furthemore add migrations and login page

--- app/Models/User.php
+++ app/Models/User.php
@@ -14,6 +14,19 @@ class User extends Authenticatable
     use HasFactory, Notifiable;
 
     /**
+     * USER ATTRIBUTES
+     * $this->attributes['id'] - int - contains the user primary key (id)
+     * $this->attributes['name'] - string - contains the user name
+     * $this->attributes['email'] - string - contains the user email
+     * $this->attributes['password'] - string - contains the user password
+     * $this->attributes['role'] - string - contains the user role
+     * $this->attributes['email_verified_at'] - datetime - contains the email verification timestamp
+     * $this->attributes['remember_token'] - string - contains the remember token for authentication
+     * $this->attributes['created_at'] - datetime - contains the user creation timestamp
+     * $this->attributes['updated_at'] - datetime - contains the user update timestamp
+     */
+
+    /**
      * The attributes that are mass assignable.
      *
      * @var list<string>
@@ -22,6 +35,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'role',
     ];
 
     /**
@@ -45,5 +59,95 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    // public function bills(): HasMany
+    // {
+    //     return $this->hasMany(Bill::class);
+    // }
+
+    // public function libraryItems(): HasMany
+    // {
+    //     return $this->hasMany(LibraryItem::class);
+    // }
+
+    // public function wishlistItems(): HasMany
+    // {
+    //     return $this->hasMany(WishlistItem::class);
+    // }
+
+    public function getId(): int
+    {
+        return $this->attributes['id'];
+    }
+
+    public function setId(int $id): void
+    {
+        $this->attributes['id'] = $id;
+    }
+
+    public function getName(): string
+    {
+        return $this->attributes['name'];
+    }
+
+    public function setName(string $name): void
+    {
+        $this->attributes['name'] = $name;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->attributes['email'];
+    }
+
+    public function setEmail(string $email): void
+    {
+        $this->attributes['email'] = $email;
+    }
+
+    public function getPassword(): string
+    {
+        return $this->attributes['password'];
+    }
+
+    public function setPassword(string $password): void
+    {
+        $this->attributes['password'] = $password;
+    }
+
+    public function getRole(): string
+    {
+        return $this->attributes['role'];
+    }
+
+    public function setRole(string $role): void
+    {
+        $this->attributes['role'] = $role;
+    }
+
+    public function getEmailVerifiedAt(): ?\DateTime
+    {
+        return $this->attributes['email_verified_at'] ?? null;
+    }
+
+    public function setEmailVerifiedAt(?\DateTime $emailVerifiedAt): void
+    {
+        $this->attributes['email_verified_at'] = $emailVerifiedAt;
+    }
+
+    public function getRememberToken(): ?string
+    {
+        return $this->attributes['remember_token'] ?? null;
+    }
+
+    public function getCreatedAt(): ?\DateTime
+    {
+        return $this->attributes['created_at'] ?? null;
+    }
+
+    public function getUpdatedAt(): ?\DateTime
+    {
+        return $this->attributes['updated_at'] ?? null;
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,3 +3,6 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'App\Http\Controllers\MovieController@index')->name('movie.index');
+
+Route::get('/users', 'App\Http\Controllers\UserController@index')->name('user.index');
+Route::post('/users', 'App\Http\Controllers\UserController@create')->name('user.create');
